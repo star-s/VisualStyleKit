eccf251bd98c1e55eaae750e42817222d0fc1f64
Sergey Starukhin <star.s@me.com>
2021-08-28 15:02:36 +0200

    refactoring

--- Sources/VisualStyleKit/Text/NSAttributedStringExt.swift
+++ Sources/VisualStyleKit/Text/NSAttributedStringExt.swift
@@ -9,37 +9,69 @@ import UIKit
 
 extension NSAttributedString {
 	
-	public static func make(with image: UIImage, capitalCharacterHeight: CGFloat? = nil) -> NSAttributedString {
-		NSAttributedString(attachment: .make(with: image, capitalCharacterHeight: capitalCharacterHeight))
-	}
-	
-	public static func make(space width: CGFloat) -> NSAttributedString {
+	public static func space(width: CGFloat) -> NSAttributedString {
 		let attach = NSTextAttachment()
 		attach.bounds.size.width = width
 		return NSAttributedString(attachment: attach)
 	}
 	
-	public func append(space width: CGFloat) -> NSAttributedString {
-		modify {
-			$0.append(.make(space: width))
+	public func appended(space width: CGFloat) -> NSAttributedString {
+		modified {
+			$0.append(.space(width: width))
 		}
 	}
 	
-	public func append(text attrString: NSAttributedString) -> NSAttributedString {
-		modify {
+	public func appended(text attrString: NSAttributedString) -> NSAttributedString {
+		modified {
 			$0.append(attrString)
 		}
 	}
 	
-	public func append(image: UIImage, capitalCharacterHeight: CGFloat? = nil) -> NSAttributedString {
-		modify {
-			$0.append(.make(with: image, capitalCharacterHeight: capitalCharacterHeight))
+	public func appended(image: UIImage, capitalCharacterHeight: CGFloat? = nil) -> NSAttributedString {
+		modified {
+			$0.append(image.embeddedInString(capitalCharacterHeight: capitalCharacterHeight))
 		}
 	}
 	
-	func modify(_ modificator: (NSMutableAttributedString) -> Void) -> NSAttributedString {
+	func modified(_ modificator: (NSMutableAttributedString) -> Void) -> NSAttributedString {
 		let result = NSMutableAttributedString(attributedString: self)
 		modificator(result)
 		return NSAttributedString(attributedString: result)
+	}
+}
+
+public extension UIImage {
+	
+	func embeddedInString(capitalCharacterHeight: CGFloat? = nil) -> NSAttributedString {
+		let attach = NSTextAttachment()
+		attach.image = self
+		attach.bounds.size = size
+		capitalCharacterHeight.map {
+			attach.bounds.origin.y = ( $0 - attach.bounds.height ) / 2
+		}
+		return NSAttributedString(attachment: attach)
+	}
+}
+
+public extension String {
+	
+	func attributed(_ attributes: [NSAttributedString.Key: Any]? = nil) -> NSAttributedString {
+		NSAttributedString(string: self, attributes: attributes)
+	}
+
+	func attributed(_ block: (inout [NSAttributedString.Key: Any]) -> Void) -> NSAttributedString {
+		var attributes = [NSAttributedString.Key: Any]()
+		block(&attributes)
+		return NSAttributedString(string: self, attributes: attributes)
+	}
+}
+
+@available(iOS 10.0, tvOS 10.0, *)
+extension Array where Element: NSAttributedString {
+	
+	func join(_ traitCollection: UITraitCollection? = nil) -> NSAttributedString {
+		let result = NSMutableAttributedString()
+		(traitCollection?.layoutDirection == .rightToLeft ? reversed() : self).forEach(result.append)
+		return result
 	}
 }
